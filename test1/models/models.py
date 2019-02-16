7c7cc877b15b9603f0c6135e6e870adf7866d3fe
Dmitriy Posypaiko <dima.posypajko@gmail.com>
2019-02-16 21:53:26 +0200

    Continue

--- test1/models/models.py
+++ test1/models/models.py
@@ -13,51 +13,48 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
-class Puppers(models.Model):
-    _name = 'puppers.puppers'
-    name = fields.Char(string='Name', size= 256, track_visibility='onchange')
-    year = fields.Integer(string='Year')
-    gender = fields.Selection([('male', 'male'),('doggess', 'doggess')],
-                                string='Gender')
-    parents_id = fields.One2many(comodel_name='dog.dog',
-                                inverse_name="pupper_id",
-                                string="Parents")
+class Partner(models.Model):
+    _inherit = 'res.partner'
 
-class Owner(models.Model):
-    _name = 'owner.owner'
-    # firstname = fields.Char(string='firstname', size= 256, track_visibility='onchange')
-    # lastname = fields.Char(string='lastname', size= 256, track_visibility='onchange')
-    owner = fields.Many2one(comodel_name='res.partner', string='Owner')
-    dog_id = fields.One2many(comodel_name='dog.dog', ondelete="cascade",
-                            inverse_name="owner_id", string="Dog")
-    date_start = fields.Date(string='Start date',
-                            default=fields.Date.context_today,
-                            track_visibility='onchange')
-    date_end = fields.Date(string='End date',
-                            default=fields.Date.context_today,
-                            track_visibility='onchange')
-    duration = fields.Integer(string='Duration',
-                              compute='_compute_duration')
-    info = fields.Char(string='Owner',track_visibility='onchange')
+    pets_ids = fields.One2many('test1.dog', string='Pets', inverse_name='owner_id')
 
-    @api.depends('date_start', 'date_end')
-    def _compute_duration(self):
-                if self.date_start and self.date_end:
-                    start_date = fields.Datetime.from_string(self.date_start)
-                    end_date = fields.Datetime.from_string(self.date_end)
-                    self.duration = (end_date - start_date).days + 1
 
 class Dog(models.Model):
-    _name = 'dog.dog'
-    _inherit = 'puppers.puppers'
-    owner_id = fields.Many2one(comodel_name='owner.owner',string='Owner')
-    pupper_id = fields.Many2one(comodel_name='puppers.puppers', string='Pupper')
+    _name = 'test1.dog'
+
+    name = fields.Char(string='Name', size=256, track_visibility='onchange', required='True')
+    mather_id = fields.Many2one('test1.dog', 'mather', domain=[('sex', '=', 'female')])
+    father_id = fields.Many2one('test1.dog', 'father', domain=[('sex', '=', 'male')])
+    owner_id = fields.Many2one('res.partner', 'Owner', required='True')
+    bersday = fields.Date('Bersday', required='True')
+    image = fields.Binary('Image', attachment=True)
+    # pupper_id = fields.Many2one(comodel_name='test1.puppers', string='Pupper')
+    sex = fields.Selection([('male', 'male'),('female', 'female')], string='Sex', required='True')
+
+
+
+    # @api.depends('date_start', 'date_end')
+    # def _compute_duration(self):
+    #             if self.date_start and self.date_end:
+    #                 start_date = fields.Datetime.from_string(self.date_start)
+    #                 end_date = fields.Datetime.from_string(self.date_end)
+    #                 self.duration = (end_date - start_date).days + 1
+
 # class Parents(models.Model):
 #     _name = 'parents.parents'
 #     _inherit = 'dog.dog'
 #
 #     pupper_id = fields.Many2one(comodel_name='puppers.puppers', string='Pupper')
 
+# class Puppers(models.Model):
+#     _name = 'test1.puppers'
+#     name = fields.Char(string='Name', size= 256, track_visibility='onchange')
+#     year = fields.Integer(string='Year')
+#     gender = fields.Selection([('male', 'male'),('doggess', 'doggess')],
+#     string='Gender')
+#     parents_id = fields.One2many(comodel_name='test1.dog',
+#     inverse_name="pupper_id",
+#     string="Parents")
 
     # puppies_ids = fields.Many2many(comodel_name='puppies.puppies',
     #                                 ondelete="cascade",
